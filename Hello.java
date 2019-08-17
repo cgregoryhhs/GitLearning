1384ff41e1d726a41026c9f00f7fdb9b06f0b8cc
Chris Gregory <cgregory@ewrsd.k12.nj.us>
2019-08-17 16:36:00 -0400

    Initial sharing of project

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Hello here.
+ *
+ * @author Chris Gregory
+ * @version 8.17.19
+ */
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Stupid!");
+    }
+}
